f882ae1fd7690800d0603a01d55d93f17f2fe471
MAIN\thb7554 <thb7554@g.rit.edu>
2017-04-17 15:32:39 -0400

    Updated click and destorythis to allow for scoring, updated UI, changed model to be bigger to avoid exploit

--- Assets/Click.cs
+++ Assets/Click.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Click : MonoBehaviour {
     public GameObject indict;
     public Camera cam;
-	// Use this for initialization
-	void Start () {
+    public bool startedBool;
+    public bool endedBool;
+    public float score;
+    private bool scoring;
+    public Text scoreText;
+    // Use this for initialization
+    void Start () {
+        startedBool = false;
+        endedBool = false;
+        score = 0;
+        scoring = false;
 	}
 	
 	// Update is called once per frame
@@ -17,9 +27,30 @@ public class Click : MonoBehaviour {
             Instantiate(indict, new Vector3(screenCor.x, screenCor.y,0), new Quaternion());
         }
         else{
-            foreach(GameObject trail in GameObject.FindGameObjectsWithTag("Trail")){
-                trail.GetComponent<DestroyThis>().destroyBool = true;
+            if (startedBool && endedBool)
+            {
+                startedBool = false;
+                endedBool = false;
+                scoring = true;
+                score = 100;
             }
+            foreach (GameObject trail in GameObject.FindGameObjectsWithTag("Trail"))
+            {
+                //Destory nodes
+                trail.GetComponent<DestroyThis>().destroyBool = true;
+                //If the circuit is completed take a point away for every node
+                if (scoring)
+                {
+                    score--;
+                    //If a node goes outside the bounds subtract more points
+                    if (trail.GetComponent<DestroyThis>().missBool)
+                    {
+                        score -= 10;
+                    }
+                }         
+            }
+            scoreText.text = "SCORE: " + score;
         }
+        scoring = false;
 	}
 }
--- Assets/DestroyThis.cs
+++ Assets/DestroyThis.cs
@@ -2,33 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyThis : MonoBehaviour {
+public class DestroyThis : MonoBehaviour
+{
     public float Timer;
     public bool destroyBool;
     private Color originalColor;
     public Renderer rend;
-    private bool hit;
-	// Use this for initialization
-	void Start () {
-        hit = true;
+    public bool missBool;
+    public GameObject clickManager;
+    // Use this for initialization
+    void Start()
+    {
+        clickManager = GameObject.Find("GameObject");
+        missBool = false;
+        RaycastHit hit;
         rend = GetComponent<Renderer>();
-		if (Physics.Raycast(transform.position, transform.forward * 1, 100))
-           rend.material.color = originalColor;
-        else
+        if (Physics.Raycast(transform.position, transform.forward * 1, out hit))
         {
-            hit = false;
+            if (hit.transform.tag == "StartZone")
+            {
+                clickManager.GetComponent<Click>().startedBool = true;
+            }
+            else if (hit.transform.tag == "EndZone")
+            {
+                clickManager.GetComponent<Click>().endedBool = true;
+            }
         }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(!hit)
+        else
         {
-            rend.material.color = new Color(1.0f, (4*Time.time%2)/2,0.0f);
+            missBool = true;
         }
-        if(destroyBool){
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (missBool)
+        {
+            rend.material.color = new Color(1.0f, (4 * Time.time % 2) / 2, 0.0f);
+        }
+        if (destroyBool)
+        {
             Destroy(this.gameObject);
         }
-	}
+    }
 }
