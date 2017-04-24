6f19298c0c44641502470cbbdc0c83e74909ec3b
MAIN\thb7554 <thb7554@g.rit.edu>
2017-04-24 15:51:21 -0400

    New features

--- Assets/Click.cs
+++ Assets/Click.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 
 public class Click : MonoBehaviour {
     public GameObject indict;
+    public GameObject cursor;
     public Camera cam;
     public bool startedBool;
     public bool endedBool;
     public float score;
     private bool scoring;
     public Text scoreText;
+    public bool outOfBounds;
     // Use this for initialization
     void Start () {
         startedBool = false;
@@ -23,24 +25,30 @@ public class Click : MonoBehaviour {
 	void Update () {
 		if(Input.GetMouseButton(0))
         {
+            startedBool = false;
+            endedBool = false;
+            outOfBounds = false;
+            this.GetComponent<Timer>().finished = false;
             Vector3 screenCor = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, 0));
             Instantiate(indict, new Vector3(screenCor.x, screenCor.y,0), new Quaternion());
+            cursor.transform.position = screenCor + new Vector3(0,0,10);
             this.GetComponent<Timer>().boolOn = true;
         }
         else{
             this.GetComponent<Timer>().boolOn = false;
-            this.GetComponent<Timer>().finished = false;
-            if (startedBool && endedBool)
-            {
-                startedBool = false;
-                endedBool = false;
-                this.GetComponent<Timer>().finished = true;
-            }
             foreach (GameObject trail in GameObject.FindGameObjectsWithTag("Trail"))
             {
-                //Destory nodes
+                if(!outOfBounds && trail.GetComponent<DestroyThis>().missBool){
+                    outOfBounds = true;
+                }
+                //Destroy nodes
                 trail.GetComponent<DestroyThis>().destroyBool = true;     
             }
+            if (!outOfBounds && startedBool && endedBool)
+            {
+                this.GetComponent<Timer>().finished = true;
+            }
+            
         }
 	}
 }
