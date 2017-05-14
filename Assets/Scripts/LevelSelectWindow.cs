d806f88a2738443a66f19f6cb7164430581859d1
Ilan Isakov <ixi7500@rit.edu>
2017-05-14 16:47:41 -0400

    Updated Start Menu, moved scripts to their own folder, hopefully that didnt break everything

---
+++ Assets/Scripts/LevelSelectWindow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectWindow : GenericWindow {
+
+    public void ChooseLevel(int level)
+    {
+        Debug.Log("Choose level: " + level);
+        SceneManager.LoadScene("Scene" + level);
+    }
+
+    public void GoBack()
+    {
+        OnPreviousWindow();
+    }
+}
